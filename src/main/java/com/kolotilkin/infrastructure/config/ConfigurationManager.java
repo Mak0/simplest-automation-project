9336ab301c3a4cc550c3657387b0b7ea4bb6b8f3
Max Kolotilkin <maximk@wix.com>
2020-03-09 14:09:05 +0200

    Added config manager class with few basic env vars

---
+++ src/main/java/com/kolotilkin/infrastructure/config/ConfigurationManager.java
@@ -0,0 +1,35 @@
+package com.kolotilkin.infrastructure.config;
+
+public class ConfigurationManager {
+
+    private static final String WEB_DRIVER_TYPE = "browser-type";
+    private static final String SCREEN_RESOLUTION = "screenResolution";
+
+    private static final String TEST_ENV = "testEnv";
+
+    public static ConfigurationManager getInstance() {
+        return SingletonHolder.INSTANCE;
+    }
+
+    private ConfigurationManager() { }
+
+    public String getWebDriverType() {
+        return getValueOrDefault(WEB_DRIVER_TYPE, "chrome");
+    }
+
+    public String getScreenResolution() {
+        return getValueOrDefault(SCREEN_RESOLUTION, "1900x1280");
+    }
+
+    public String getTestEnv() {
+        return getValueOrDefault(TEST_ENV, "production");
+    }
+
+    private String getValueOrDefault(String envVar, String defaultValue) {
+        return (System.getenv(envVar) == null || System.getenv(envVar).isEmpty()) ? defaultValue : System.getenv(envVar);
+    }
+
+    private static final class SingletonHolder {
+        private static final ConfigurationManager INSTANCE = new ConfigurationManager();
+    }
+}
